4c9d27ce8f114baf2401072c0e52e05b48d31055
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-14 15:38:04 -0300

    organizando index e inicio

--- src/components/pages/Inicio/Inicio.jsx
+++ src/components/pages/Inicio/Inicio.jsx
@@ -1,4 +1,4 @@
-export function Inicio({ logo }) {
+function Inicio({ logo }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -7,30 +7,30 @@ export function Inicio({ logo }) {
           Em desenvolvimento...
         </p>
         <p>
-        <a
-          className="App-link"
-          href="https://github.com/adotapetorg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
-        <a
-          className="App-link"
-          href="https://instagram.com/adotapetorg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
-        <a
-          className="App-link"
-          href="https://youtube.com/@adotapetorg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Youtube
-        </a>
+          <a
+            className="App-link"
+            href="https://github.com/adotapetorg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+          <a
+            className="App-link"
+            href="https://instagram.com/adotapetorg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            className="App-link"
+            href="https://youtube.com/@adotapetorg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Youtube
+          </a>
         </p>
       </header>
     </div>
