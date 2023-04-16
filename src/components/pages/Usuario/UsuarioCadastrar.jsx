5f08f3867d6167f95ce45b6086760137f48ff856
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-16 11:41:34 -0300

    criada tela de cadastro e entrar de usuario

--- src/components/pages/Usuario/UsuarioCadastrar.jsx
+++ src/components/pages/Usuario/UsuarioCadastrar.jsx
@@ -1,7 +1,118 @@
+import { useState } from 'react'
+import Mensagem from '../../mensagem/Mensagem'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from "react-router-dom";
+
 function UsuarioCadastrar() {
-    return (
-      <div>UsuarioCadastrar</div>
-    )
+  const navigate = useNavigate()
+
+  const [nome, setNome] = useState("")
+  const [sobrenome, setSobrenome] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [senhaRepetida, setSenhaRepetida] = useState("")
+  const [msg, setMsg] = useState("")
+  const [msgTipo, setMsgTipo] = useState("warning")
+
+  function validaCampos() {
+    if (nome === "" || nome === null) {
+      setMsg("Preencha o campo nome")
+      return false
+    }
+
+    if (sobrenome === "" || sobrenome === null) {
+      setMsg("Preencha o campo sobrenome")
+      return false
+    }
+
+    if (email === "" || email === null) {
+      setMsg("Preencha o campo email")
+      return false
+    }
+
+    if (senha === "" || senha === null) {
+      setMsg("Preencha a campo senha")
+      return false
+    }
+
+    if (senhaRepetida === "" || senhaRepetida === null) {
+      setMsg("Preencha o campo repetir senha")
+      return false
+    }
+
+    if (senha !== senhaRepetida) {
+      setMsg("As senhas estão diferentes")
+      return false
+    }
+  }
+
+  function limpaCampos() {
+    setNome("")
+    setSobrenome("")
+    setEmail("")
+    setSenha("")
+    setSenhaRepetida("")
+    setMsg("")
+    setMsgTipo("")
+    navigate("/usuario/entrar")
+  }
+
+  return (
+    <Form>
+      <Mensagem mensagem={msg} mensagemTipo={msgTipo} />
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="nome">Nome</Form.Label>
+        <Form.Control id="nome" type="text" placeholder="Digite seu nome" value={nome} required autoFocus
+          onChange={(e) => {
+            setNome(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="sobrenome">Sobrenome</Form.Label>
+        <Form.Control id="sobrenome" type="text" placeholder="Digite seu sobrenome" value={sobrenome} required
+          onChange={(e) => {
+            setSobrenome(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="email">E-mail</Form.Label>
+        <Form.Control id="email" type="email" placeholder="Enter email" value={email} required
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="senha">Senha</Form.Label>
+        <Form.Control id="senha" type="password" placeholder="Password" value={senha} required
+          onChange={(e) => {
+            setSenha(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="senhaRepetida">Repetir senha</Form.Label>
+        <Form.Control id="senhaRepetida" type="password" placeholder="Password" value={senhaRepetida} required
+          onChange={(e) => {
+            setSenhaRepetida(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Button variant="primary" type="submit"
+        onClick={(e) => {
+          e.preventDefault()
+          validaCampos()
+          limpaCampos()
+          // enviar dados para API
+        }}>
+        Cadastrar
+      </Button>
+    </Form>
+  );
 }
 
-export default UsuarioCadastrar
+export default UsuarioCadastrar;
--- src/components/pages/Usuario/UsuarioEntrar.jsx
+++ src/components/pages/Usuario/UsuarioEntrar.jsx
@@ -1,7 +1,72 @@
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Mensagem from './../../mensagem/Mensagem';
+import { useNavigate } from "react-router-dom";
+
 function UsuarioEntrar() {
-    return (
-      <div>UsuarioEntrar</div>
-    )
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [msg, setMsg] = useState("")
+  const [msgTipo, setMsgTipo] = useState("warning")
+
+  function validaCampos() {
+    if (email === "" || email === null) {
+      setMsg("Preencha o campo email")
+      return false
+    }
+
+    if (senha === "" || senha === null) {
+      setMsg("Preencha a campo senha")
+      return false
+    }
+  }
+
+  function limpaCampos() {
+    setEmail("")
+    setSenha("")
+    setMsg("")
+    setMsgTipo("")
+    navigate("/")
+  }
+
+  return (
+    <Form>
+      <Mensagem mensagem={msg} mensagemTipo={msgTipo} />
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="email">E-mail</Form.Label>
+        <Form.Control id="email" type="email" placeholder="Enter email" value={email} required
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="senha">Senha</Form.Label>
+        <Form.Control id="senha" type="password" placeholder="Password" value={senha} required
+          onChange={(e) => {
+            setSenha(e.target.value)
+          }} />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Lembre-me" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit"
+        onClick={(e) => {
+          e.preventDefault()
+          validaCampos()
+          limpaCampos()
+          // enviar dados para API
+        }}>
+          Entrar
+        </Button>
+    </Form>
+  );
 }
 
-export default UsuarioEntrar
+export default UsuarioEntrar;
