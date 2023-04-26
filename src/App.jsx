2dfb84fbabd7a1f75e9176b4f5341c897e63a47b
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-26 17:46:45 -0300

    add btnVoltarTopo

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,20 @@ import { Container } from 'react-bootstrap';
 import logo from './images/logo.jpg';
 import UsuarioCadastrar from './components/pages/usuario/UsuarioCadastrar';
 import UsuarioEntrar from './components/pages/usuario/UsuarioEntrar';
+import BtnVoltarTopo from './components/buttons/BtnVoltarTopo';
 
 function App() {
   return (
     <BrowserRouter>
       <Header logo={logo} />
-      <Container>
+      <Container id="container">
         <Routes>
           <Route path="/" element={<Inicio logo={logo} />} />
           <Route path="/sobre" element={<Sobre />} />
           <Route path="/usuario/cadastrar" element={<UsuarioCadastrar />} />
           <Route path="/usuario/entrar" element={<UsuarioEntrar />} />
         </Routes>
+        <BtnVoltarTopo />
       </Container>
     </BrowserRouter>
   )
