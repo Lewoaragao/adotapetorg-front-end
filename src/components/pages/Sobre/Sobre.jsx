1a57ae1dfd0215a42b11292edb9d098b0432a41b
Leonardo Aragão <lewoaragao@gmail.com>
2023-04-28 09:13:41 -0300

    criando page sobre

--- src/components/pages/Sobre/Sobre.jsx
+++ src/components/pages/Sobre/Sobre.jsx
@@ -1,7 +1,16 @@
 function Sobre() {
-        return (
-            <div>Sobre</div>
-        )
+    return (
+        <div>
+            <h2 className="text-primary">Nossa história</h2>
+            <p>Tudo começou com um casal que ama muito os animais, em especial nossa cadelinha chamada Chandelly (sim sim, o nome faz referência aquele danone) e ela faz jus ao nome, pois ela é um docinho, com esse amor pelos animais crescendo, acabou que resolvi desenvolver um sistema no qual ONG, Empresa ou pessoas que tenham interesse em doar animais, possam cadastrá-lo no sistema e assim facilitar a adoção através do contato pela internet.</p>
+
+            <h2 className="text-primary">Sobre a organização</h2>
+            <p>Nós somos uma organização sem fins lucrativos, onde desenvolvemos um sistema para ajudar nossos amados Pets a encontrar um lar através da adoção ou reencontrar o lar através do nosso sistema de busca de animais perdidos cadastrados.</p>
+
+            <h2 className="text-primary">Data de criação</h2>
+            <p>17/03/2023</p>
+        </div>
+    )
 }
 
 export default Sobre
