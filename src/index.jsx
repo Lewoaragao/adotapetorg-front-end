dd6ede0cfaeae56102b292d65876cf8fb6358608
Leonardo Aragão <lewoaragao@gmail.com>
2023-05-02 17:55:21 -0300

    explicação axios fazendo duas requisições

--- src/index.jsx
+++ src/index.jsx
@@ -27,6 +27,24 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  /**
+   * AXIOS fazendo duas requisições ao invés de uma
+   * 
+   * Explicação:
+   * O <React.StrictMode> é um recurso do React que ajuda a identificar possíveis problemas em seu código e a 
+   * garantir que seu código esteja seguindo as melhores práticas recomendadas pelo React. Ele faz isso executando certas verificações extras em seu código, que podem 
+   * resultar em renderizações adicionais.
+   * Uma das verificações adicionais que o <React.StrictMode> executa é executar o render duas vezes para ajudar a identificar possíveis efeitos colaterais que podem ocorrer 
+   * durante as renderizações. Como o useEffect é um hook que é acionado após a renderização do componente, é possível que ele seja executado duas vezes devido a essa 
+   * verificação adicional do <React.StrictMode>.
+   * Se você quiser desligar essa feature, pode ir ao index.tsx e remover o StrictMode.
+   * Vale apontar que isso só acontece durante o desenvolvimento, nas versões de produção o React remove isso pra gente. 
+   * 
+   * Documentação: 
+   * https://pt-br.legacy.reactjs.org/docs/strict-mode.html#gatsby-focus-wrapper
+   */
+
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
