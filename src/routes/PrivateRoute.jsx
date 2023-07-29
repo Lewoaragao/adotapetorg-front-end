0bbee5ffc63b82a2ac13fdd65bca90782cd648f5
Leonardo Aragão <lewoaragao@gmail.com>
2023-07-29 13:32:06 -0300

    correção desenv libs e rotas privadas

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,12 +1,11 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
-import { Outlet } from "react-router-dom";
 
 export default function PrivateRoute({ element }) {
   const { isUsuarioLogado } = useContext(AuthContext);
 
   return (
-    <> {isUsuarioLogado ? <Outlet /> : <Navigate to="/usuario/entrar" />}</>
+    <> {isUsuarioLogado ? element : <Navigate to="/usuario/entrar" />}</>
   );
 }
